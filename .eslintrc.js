1c29f3f4400ccdce4a606845835803dcbd759f51
fsousaentre <85347233+fredsons@users.noreply.github.com>
2022-09-15 15:51:41 -0300

    atualizacao

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,14 @@
 module.exports = {
     "env": {
-        "browser": true,
-        "es2021": true
+        "es2021": true,
+        "node": true
     },
     "extends": "eslint:recommended",
-    "overrides": [
-    ],
     "parserOptions": {
-        "ecmaVersion": "latest",
+        "ecmaVersion": 12,
         "sourceType": "module"
     },
     "rules": {
+        'no-console': 'off',
     }
-}
+};
